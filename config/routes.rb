36c735545dd8f0f16be57250e60c79357e4428f4
x_wang <18912540921@163.com>
2019-03-05 16:44:33 +0800

    resources and resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :admin do
-    resource :products
+    resources :products
     get "products/index"
   end
 
-  resource :products
+  resources :products
   get "products/index"
 
   root 'products#index'
