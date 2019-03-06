146c256b589c462eb2f4e18853303ca8f86f33fa
x_wang <18912540921@163.com>
2019-03-06 12:27:54 +0800

    annotate model info

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint(8)        not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  quantity    :integer
+#  price       :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  image       :string(255)
+#
+
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 end
